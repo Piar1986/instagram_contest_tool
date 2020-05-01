58461878d1f2aa1f26bc69c4983cb0508dd7bec7
IvanPolienko <piar1986@yandex.ru>
2020-05-01 18:12:06 +0700

    Used intersection of sets instead of functions

--- manage.py
+++ manage.py
@@ -12,7 +12,7 @@ def create_parser():
     return parser
 
 
-def get_user_mention(comment_text):
+def get_users_mentions(comment_text):
     # https://blog.jstassen.com/2016/03/code-regex-for-instagram-username-and-hashtags/
     result = re.findall("(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)", comment_text)
     return result
@@ -22,43 +22,26 @@ def is_user_exist(username):
     return bot.get_user_id_from_username(username) is not None
 
 
-def get_comments_with_friend(comments):
-    comments_with_friend = []
+def get_username(user_id):
+    return bot.get_username_from_user_id(user_id)
+
+
+def get_comments_with_friend_users_ids(comments):
+    comments_with_friend_users_ids = []
     for comment in comments:
-        usernames = get_user_mention(comment['text'])
+        comment_text = comment['text']
+        usernames = get_users_mentions(comment_text)
         for username in usernames:
             if is_user_exist(username):
-                comment_user_id = int(comment['user_id'])
-                comment_username = comment['user']['username']
-                comments_with_friend.append((comment_user_id, comment_username))
-    return comments_with_friend
+                comment_user_id = str(comment['user_id'])
+                comments_with_friend_users_ids.append(comment_user_id)
+    return comments_with_friend_users_ids
 
 
-def get_comments_with_friend_like(post_likers_users, comments):
-    comments_with_friend_like = []
-    for comment in comments:
-        for liker in post_likers_users:
-            liker_id = int(liker)
-            comment_user_id = comment[0]
-            if liker_id==comment_user_id:
-                comments_with_friend_like.append(comment)
-    return comments_with_friend_like
-
-
-def get_comments_with_friend_like_follow(follower_users, comments):
-    comments_with_friend_like_follow = []
-    for comment in comments:
-        for user in follower_users:
-            folower_id = int(user)
-            comment_user_id = comment[0]
-            if folower_id==comment_user_id:
-                comments_with_friend_like_follow.append(comment)
-    return comments_with_friend_like_follow
-
-
-def show_winners(winners):
+def show_winners_usernames(winners):
+    print('Участники, выполнившие условия конкурса:')
     for winner in winners:
-        winner_username = winner[1]
+        winner_username = get_username(winner)
         print(winner_username)
 
 
@@ -78,14 +61,16 @@ if __name__ == '__main__':
     bot = Bot()
     bot.login(username=login, password=password)
     
-    media_id = bot.get_media_id_from_link(args.post_link)
-    comments = bot.get_media_comments_all(media_id)
-    post_likers_users = bot.get_media_likers(media_id)
-    follower_users = bot.get_user_followers(args.post_author)
+    post_media_id = bot.get_media_id_from_link(args.post_link)
+    post_comments = bot.get_media_comments_all(post_media_id)
     
-    comments_with_friend = get_comments_with_friend(comments)
-    comments_with_friend_like = get_comments_with_friend_like(post_likers_users, comments_with_friend)
-    comments_with_friend_like_follow = get_comments_with_friend_like_follow(follower_users, comments_with_friend_like)
-    
-    winners = set(comments_with_friend_like_follow)
-    show_winners(winners)
+    comments_with_friend_users_ids = get_comments_with_friend_users_ids(post_comments)
+    post_likers_users_ids = bot.get_media_likers(post_media_id)
+    followers_users_ids = bot.get_user_followers(args.post_author)
+        
+    comments_with_friend_users_ids_set = set(comments_with_friend_users_ids)
+    post_likers_users_ids_set = set(post_likers_users_ids)
+    followers_users_ids_set = set(followers_users_ids)
+
+    winners_users_ids = comments_with_friend_users_ids_set & post_likers_users_ids_set & followers_users_ids_set
+    show_winners_usernames(winners_users_ids)
